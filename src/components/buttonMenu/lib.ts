2b75fa80c272539ca6dd366b6cd14a89373a2dd3
Miroslav Grela <m.grela@quadient.com>
2017-10-10 08:41:25 +0200

    ButtonMenu refactoring.

--- src/components/buttonMenu/lib.ts
+++ src/components/buttonMenu/lib.ts
@@ -17,37 +17,38 @@ interface IComponentCtx extends b.IBobrilCtx {
 export const create = b.createComponent<IComponentData>({
     render(ctx: IComponentCtx, me: b.IBobrilNode) {
         const d = ctx.data;
-        me.children = getList(ctx);
+        me.children = b.styledDiv([
+            createButton(ctx),
+            ...createMenuItems(ctx)
+        ], getMenuStyle());
         b.style(me, { width: 200 });
     }
 });
 
-function getList(ctx: IComponentCtx): b.IBobrilChildren {
-    let children = [
-        m.Button({ action: () => {
-            ctx.expanded = !ctx.expanded;
-            b.invalidate();
-        }}, icons.navigationMenu()),
-    ];
+function createButton(ctx: IComponentCtx): b.IBobrilNode {
+    return m.Button({ action: () => {
+        ctx.expanded = !ctx.expanded;
+        b.invalidate();
+    }}, icons.navigationMenu());
+}
 
-    if (ctx.expanded && ctx.data.children) {
-        let items = [
-            m.Divider({}),
-            ...ctx.data.children
-                .map(child => m.ListItem({ action: () => { 
-                    ctx.expanded = !ctx.expanded; 
-                    child.data && child.data.action && child.data.action(); 
-                }}, child))
-        ];
-        children.push(m.List({}, items));
-    }
+function createMenuItems(ctx: IComponentCtx): b.IBobrilNode[] {
+    return ctx.expanded && ctx.data.children
+    ? [ 
+        m.Divider({}),
+        ...ctx.data.children
+            .map(child => m.ListItem({ action: () => { 
+                ctx.expanded = !ctx.expanded; 
+                child.data && child.data.action && child.data.action(); 
+            }}, child))
+    ]
+    : [];
+}
 
-    return b.styledDiv(
-        children, 
-        { 
-            margin: 16,
-            color: m.primary1Color,
-            backgroundColor: m.canvasColor
-        }
-    );
+function getMenuStyle(): string {
+    return b.styleDef({
+        margin: 16,
+        color: m.primary1Color,
+        backgroundColor: m.canvasColor
+    });
 }
